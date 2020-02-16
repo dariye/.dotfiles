c0709349f650b949e9adf2df04ba59fb3d041db4
paul <paul.dariye@gmail.com>
2020-02-16 21:32:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# .dotfiles
+
+
+## Installation
+
+```
+git clone git@github.com:pauldariye/.dotfiles.git # clone
+./install.sh # setup environment
+
+```
+
+## Programs
+
+### Shell
+
+- fish - fisher as pkg manager
+
+### Editors
+
+- neovim powered by spacevim
+- code
+
+### Package managers
+
+- brew
+- asdf
+
+### Dotfile manager
+
+- rcm
+
---
+++ config/fish/config.fish
@@ -0,0 +1,19 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set --local BREW_PREFIX (brew --prefix)
+
+set -g fish_user_paths "/usr/local/opt/gettext/bin" (yarn global bin) $fish_user_paths
+
+[ -f $BREW_PREFIX/opt/asdf/asdf.fish ]
+and source $BREW_PREFIX/opt/asdf/asdf.fish
+
+if type nvim >/dev/null 2>/dev/null
+  alias vim='nvim'
+  alias vi='nvim'
+end
+
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
